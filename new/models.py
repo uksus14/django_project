e4b67b4ad85a33f3a805d1c8f3a0948e5e6b8eea
uksus14 <72032537+uksus14@users.noreply.github.com>
2022-05-22 05:35:25 +0600

    first trying models

--- new/models.py
+++ new/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django import forms
+from project_settings import *
 
-# Create your models here.
+class Tutor(): pass
+class Club(): pass
+class Faculty(): pass
+class Course(): pass
+class Subject(): pass
+
+class User(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    email = models.EmailField()
+    password = models.IntegerField()
+    is_superuser = models.IntegerField()
+
+    second_name = models.CharField(max_length=2**10)
+    first_name = models.CharField(max_length=2**10)
+
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL)
+    tutors = models.ManyToManyField(Tutor)
+    clubs = models.ManyToManyField(Club, primary_key=True)
+
+
+class Subject():
+    subject_name = models.CharField(max_length=64)
+    faculties = models.ManyToManyField(Faculty, )
+
+
+class Course():
+    faculty_name = models.ForeignKey(Faculty, on_delete=models.SET_NULL)
+    year = models.IntegerField()
+    group = models.IntegerField()
+    students = models.ManyToManyField(User)
+
+
+class Club():
+    students = models.ManyToManyField(User)
+    club_name = models.CharField(max_length=32, primary_key=True)
+
+
+class Faculty():
+    students = models.ManyToManyField(User)
+    faculty_name = models.CharField(max_length=8)
+    subjects = models.ManyToManyField(Subject, on_delete=models.DO_NOTHING)
+
+
+class Tutor(models.Model):
+    subject = forms.ChoiceField(choices=TUTOR_SUBJECTS)
+    student_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    tuties = models.
